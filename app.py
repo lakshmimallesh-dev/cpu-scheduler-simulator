c1ab052d02d7f20ab51fa5cd7621c4fcdf7f2a16
S. Lakshmi Mallesh <lakshmimalleshsuvarnam@gmail.com>
2026-04-16 17:04:42 +0530

    fix port binding for deployment

--- app.py
+++ app.py
@@ -432,5 +432,8 @@ def ask():
 
     return jsonify({"answer": "Bro ask something related to CPU scheduling 😄"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
